1d5bbcf3855536a5d8d60f66c401b99a28336d60
chaoxin <chaoxin13@hotmail.com>
2017-03-12 13:16:06 +0100

    Stap3 Pimped Up

--- SpaceInvadersT/SpaceInvadersT/PlayingState.cs
+++ SpaceInvadersT/SpaceInvadersT/PlayingState.cs
@@ -38,52 +38,25 @@ namespace SpaceInvadersT
                 Invaders blueInvaders = new Invaders("blue_invader");
                 blueInvaders.Position = new Vector2(totalInvaders * 60, 20);
                 blueInvaders.Velocity = new Vector2(4);
-                
-                invaders.Add(blueInvaders);
-
-                
-
-            }
-            for (int totalInvaders = 0; totalInvaders < 9; totalInvaders++)
-            {
-
 
                 Invaders greenInvaders = new Invaders("green_invader");
                 greenInvaders.Position = new Vector2(totalInvaders * 60, 70);
                 greenInvaders.Velocity = new Vector2(3);
 
-                invaders.Add(greenInvaders);
-
-
-
-            }
-            for (int totalInvaders = 0; totalInvaders < 9; totalInvaders++)
-            {
-
-
                 Invaders yellowInvaders = new Invaders("yellow_invader");
                 yellowInvaders.Position = new Vector2(totalInvaders * 60, 120);
                 yellowInvaders.Velocity = new Vector2(2);
-
-                invaders.Add(yellowInvaders);
-
-
-
-            }
-
-            for (int totalInvaders = 0; totalInvaders < 9; totalInvaders++)
-            {
-
 
                 Invaders redInvaders = new Invaders("red_invader");
                 redInvaders.Position = new Vector2(totalInvaders * 60, 170);
                 redInvaders.Velocity = new Vector2(1);
 
+                invaders.Add(blueInvaders);
+                invaders.Add(greenInvaders);
+                invaders.Add(yellowInvaders);
                 invaders.Add(redInvaders);
-
-
-
             }
+            
 
             this.Add(background);
             this.Add(player);
